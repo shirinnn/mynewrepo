e89be11eaa46ba72ebe86b387cbfc28b4cdfcddc
Shirin <shirinoh88@gmail.com>
2019-06-05 23:49:51 +0800

    Working Firebase CRUD

--- angular7firestore/src/app/app.module.ts
+++ angular7firestore/src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { environment } from '../environments/environment';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeService } from './shared/employee.service';
+import { FormsModule } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    EmployeeListComponent,
+    EmployeesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [EmployeeService, AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular7firestore/src/environments/environment.ts
+++ angular7firestore/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDvnPaIEKW4fSDkZd76iQm85unvQuDT0f4",
+    authDomain: "fir-crudangular7.firebaseapp.com",
+    databaseURL: "https://fir-crudangular7.firebaseio.com",
+    projectId: "fir-crudangular7",
+    storageBucket: "fir-crudangular7.appspot.com",
+    messagingSenderId: "76589515772",
+    appId: "1:76589515772:web:2443ab5d7b3716b8"
+  }
 };
 
 /*
